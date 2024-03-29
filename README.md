3337a525804311685e3d7531ec231f932a5c71de
Tim Payne <ma7tcsp@gmail.com>
2024-03-29 14:48:30 +0000

    Refresh for TC24

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # emb-hot-ca-v3
 
-Course material for Embedded Hands-on-Training.
+Course material for Embedded Hands-on-Training for TC24
 The course covers Connected Apps and the Embedding API v3
--- static/js/embed9.js
+++
@@ -1,8 +0,0 @@
-// fires when all html css and images are loaded, but doesn't wait for iframe content
-window.onload = function () {
-   viz = document.getElementById('tableauViz');
-   console.log(viz);
-   window.location.href = '/complete';
-   // call the ready() function when the tableau iframe is ready to interact with and fully loaded
-
-   }
