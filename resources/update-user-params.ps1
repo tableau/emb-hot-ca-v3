962e74e8fb6f7cecb8b4a091722298f382caeb6a
Tim Payne <47423639+ma7tcsp@users.noreply.github.com>
2024-04-02 18:40:58 +0100

    Update update-user-params.ps1

--- resources/update-user-params.ps1
+++ resources/update-user-params.ps1
@@ -1,2 +1,2 @@
 $tabUser = get-content 'c:\Users\Administrator\labUser.txt'
-((Get-Content -path C:\Users\Administrator\Documents\HOT1860\emb-hot-ca-v3-master\EmbedPortal.py -Raw) -replace 'user_default',$tabUser) | Set-Content -Path C:\Users\Administrator\Documents\HOT1860\tab-emb-ca-v3-master\EmbedPortal.py
+((Get-Content -path C:\Users\Administrator\Documents\HOT1860\emb-hot-ca-v3-tc24\EmbedPortal.py -Raw) -replace 'user_default',$tabUser) | Set-Content -Path C:\Users\Administrator\Documents\HOT1860\emb-hot-ca-v3-tc24\EmbedPortal.py
